ab7a4348e9bc42b867f199451512845ea2e194bb
halil <halil4deen@gmail.com>
2019-09-13 10:45:40 +0100

    addead a DateTimeField to the models.py file so as to sort tasks by the most recent

--- todolist_app/models.py
+++ todolist_app/models.py
@@ -5,6 +5,10 @@ from django.db import models
 class TaskList(models.Model):
     task = models.CharField(max_length=300)
     done = models.BooleanField(default=False)
+    created_date = models.DateTimeField(editable=False, auto_now=True)
 
     def __str__(self):
         return self.task + " - " + str(self.done)
+
+    class Meta:
+        ordering = ('-created_date')
